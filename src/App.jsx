87a25aede9128d2f2245f89525b40743502ce9a7
RajatDahiya <dahiyarajat528@gmail.com>
2021-07-19 19:15:48 +0530

    First dry run react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Rajat from "./Components/user";
+
+
+function App() {
+  return (
+    <div>
+      <h1>User</h1>
+      <Rajat />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/user.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function User() {
+    return (
+        <div>
+            <h1>Rajat</h1>
+            <h4>full Stack</h4>
+        </div>
+    );
+};
+
+export default User;
