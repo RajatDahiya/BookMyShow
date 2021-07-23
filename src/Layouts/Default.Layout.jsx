d1f2849170a8148701916799c975544fd69508e9
RajatDahiya <dahiyarajat528@gmail.com>
2021-07-23 15:36:51 +0530

    Add HeroCarousel

--- src/Layouts/Default.Layout.jsx
+++ src/Layouts/Default.Layout.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+
+
+//Components
+import HeroCarousel from '../components/HeroCarousel/HeroCarousel.Component';
 import Navbar from '../components/navbar/Navbar.Component';
 
 const DefaultLayout = (props) => {
     return (
         <>
             <Navbar />
+            <HeroCarousel />
             {props.children}
             
         </>
